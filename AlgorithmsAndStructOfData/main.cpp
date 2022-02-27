df60f4fef637a5aed78cdc9cadd08487c51902b6
euglYA <kronomplay@bk.ru>
2022-02-28 02:32:44 +0700

    Added functions for sorting

--- AlgorithmsAndStructOfData/main.cpp
+++ AlgorithmsAndStructOfData/main.cpp
@@ -1,12 +1,171 @@
 #include <iostream>
 #include "filelib.h"
 
-void sortFile(const char* fileName) {
-    std::ifstream f1;
-    std::ifstream f2;
-    std::ofstream f3;
-    std::ofstream f4;
+bool checkForEmpty(const char* file1, const char* file2, const char* file3, const char* file4) {
+    std::ifstream file;
+    int count = 0;
 
+    file.open(file1);
+    if (file.peek() == EOF) count++;
+    file.close();
+
+    file.open(file2);
+    if (file.peek() == EOF) count++;
+    file.close();
+
+    file.open(file3);
+    if (file.peek() == EOF) count++;
+    file.close();
+
+    file.open(file4);
+    if (file.peek() == EOF) count++;
+    file.close();
+
+    if (count == 3)
+        return true;
+
+    return false;
+}
+
+void split(const char* fileName, const char* fileOut1, const char* fileOut2) {
+    std::ifstream file(fileName);
+    std::ofstream F[2];
+    F[0].open(fileOut1);
+    F[1].open(fileOut2);
+
+    int x1, x2, n = 0;
+    file >> x1;
+
+    while (!file.eof()) {
+        F[n] << x1 << " ";
+        file >> x2;
+
+        if (x1 > x2)
+            n = 1 - n;
+
+        x1 = x2;
+    }
+    file.close();
+    F[0].close(); F[1].close();
+}
+
+void merge(const char* fileIn1, const char* fileIn2, const char* fileOut1, const char* fileOut2) {
+    std::ifstream f1(fileIn1), f2(fileIn2);
+    std::ofstream s1(fileOut1), s2(fileOut2);
+
+    int x1, x2, y1, y2;
+
+    f1 >> y1;
+    f2 >> y2;
+    
+    while (!f1.eof() && !f2.eof()){
+        x1 = y1;
+        x2 = y2;
+        
+        while (x1 <= y1 && x2 <= y2 && !f1.eof() && !f2.eof()){
+            
+            if (y1 < y2){
+                s1 << y1 << " ";
+                x1 = y1;
+                f1 >> y1;
+            }
+            else{
+                s1 << y2 << " ";
+                x2 = y2;
+                f2 >> y2;
+            }
+        }
+
+        while (x1 <= y1 && !f1.eof()){
+            s1 << y1 << " ";
+            x1 = y1;
+            f1 >> y1;
+        }
+
+        while (x2 <= y2 && !f2.eof()){
+            s1 << y2 << " ";
+            x2 = y2;
+            f2 >> y2;
+        }
+
+        //---
+
+        x1 = y1;
+        x2 = y2;
+
+        while (x1 <= y1 && x2 <= y2 && !f1.eof() && !f2.eof()){
+            
+            if (y1 < y2){
+                s2 << y1 << " ";
+                x1 = y1;
+                f1 >> y1;
+            }
+            else{
+                s2 << y2 << " ";
+                x2 = y2;
+                f2 >> y2;
+            }
+        }
+
+        while (x1 <= y1 && !f1.eof()){
+            s2 << y1 << " ";
+            x1 = y1;
+            f1 >> y1;
+        }
+
+        while (x2 <= y2 && !f1.eof()){
+            s2 << y2 << " ";
+            x2 = y2;
+            f2 >> y2;
+        }
+
+    }
+
+    while (!f1.eof()){
+        s1 << y1 << " ";
+        f1 >> y1;
+    }
+
+    while (!f2.eof()){
+        s2 << y2 << " ";
+        f2 >> y2;
+    }
+
+    f1.close(); f2.close();
+    s1.close(); s2.close();
+
+    s1.open(fileIn1, std::ios_base::trunc);
+    s2.open(fileIn2, std::ios_base::trunc);
+    s1.close(); s2.close();
+}
+
+void sort(const char* fileName) {
+    split(fileName, "f1.txt", "f2.txt");
+
+    while (!checkForEmpty("f1.txt", "f2.txt", "s1.txt", "s2.txt")) {
+        merge("f1.txt", "f2.txt", "s1.txt", "s2.txt");
+        merge("s1.txt", "s2.txt", "f1.txt", "f2.txt");
+    }
+}
+
+const char* whatFileIsSorted(const char* file1, const char* file2, const char* file3, const char* file4) {
+    std::ifstream file;
+    
+    file.open(file1);
+    if (!(file.peek() == EOF)) return file1;
+    file.close();
+
+    file.open(file2);
+    if (!(file.peek() == EOF)) return file2;
+    file.close();
+
+    file.open(file3);
+    if (!(file.peek() == EOF)) return file3;
+    file.close();
+
+    file.open(file4);
+    if (!(file.peek() == EOF)) return file4;
+    file.close();
 
 }
 
@@ -14,9 +173,15 @@ int createAndSortFile(const char* fileName, const int numbersCount, const int ma
     if (!createFileWithRandomNumbers(fileName, numbersCount, maxNumberValue))
         return -1;
 
-    sortFile(fileName);
+    int x = 0;
+    std::cout << "start? ";
+    std::cin >> x;
 
-    if (!isFileContainsSortedArray(fileName))
+    sort(fileName);
+
+    std::cout << "File " << whatFileIsSorted("f1.txt", "f2.txt", "s1.txt", "s2.txt") << " is sorted" << std::endl;
+
+    if (!isFileContainsSortedArray(whatFileIsSorted("f1.txt", "f2.txt", "s1.txt", "s2.txt")))
         return -2;
 
     return 1;
@@ -24,23 +189,21 @@ int createAndSortFile(const char* fileName, const int numbersCount, const int ma
 
 int main() {
     const char* fileName = "file.txt";
-    const int numbersCount = 1000000;
-    const int maxNumberValue = 100000;
+    const int numbersCount = 10;
+    const int maxNumberValue = 10;
 
-    for (int i = 0; i < 10; i++) {
-        switch (createAndSortFile(fileName, numbersCount, maxNumberValue)) {
-        case 1:
-            std::cout << "Test passed." << std::endl;
-            break;
+    switch (createAndSortFile(fileName, numbersCount, maxNumberValue)) {
+    case 1:
+        std::cout << "Test passed." << std::endl;
+        break;
 
-        case -1:
-            std::cout << "Test failed: can't create file." << std::endl;
-            break;
+    case -1:
+        std::cout << "Test failed: can't create file." << std::endl;
+        break;
 
-        case -2:
-            std::cout << "test failed: file isn't sorted." << std::endl;
-            break;
-        }
+    case -2:
+        std::cout << "test failed: file isn't sorted." << std::endl;
+        break;
     }
 
     return 0;
